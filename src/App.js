07a28a5f397bd159be3e944e8d0a315d3989e5f8
Elisa Bjorg <davidsson.elisa@gmail.com>
2025-03-01 20:32:44 +0000

    Fix Tailwind CSS installation and configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          MARTA APP
-        </p>
+        <p>MARTA APP HALLA</p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
